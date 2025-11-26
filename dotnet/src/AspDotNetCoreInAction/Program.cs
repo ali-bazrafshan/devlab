188dc97ca5b7a535695834ed9c547c77b3a75274
Ali Bazrafshan <ali@bazrafshan.net>
2025-11-26 16:16:11 +0330

    AspDotNetCoreInAction: Add new endpoints

--- dotnet/src/AspDotNetCoreInAction/Program.cs
+++ dotnet/src/AspDotNetCoreInAction/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hey");
+app.MapGet("/person", () => new Person("Ali", "Bazrafshan"));
+app.MapGet("/person/{firstName}/{lastName}", (string firstName, string lastName) => new Person(firstName, lastName));
 
 app.Run();
+
+public record Person(string FirstName, string LastName);
