78b01c68384d0301524663cc735f65cbe888847d
Ali Bazrafshan <ali@bazrafshan.net>
2025-12-06 14:50:16 +0330

    A minimal API demo in memory

--- dotnet/src/AspDotNetCoreInAction/Program.cs
+++ dotnet/src/AspDotNetCoreInAction/Program.cs
@@ -1,12 +1,33 @@
-using Microsoft.AspNetCore.Identity;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hey");
-app.MapGet("/person", () => new Person("Ali", "Bazrafshan"));
-app.MapGet("/person/{firstName}/{lastName}", (string firstName, string lastName) => new Person(firstName, lastName));
+app.MapGet("/", () => "Hello.");
+app.MapGet("/person", () => Person.All);
+app.MapGet("/person/{id}", (int id) => Person.All[id]);
+app.MapPost("/person", Handlers.AddPerson);
+app.MapPut("/person", Handlers.ReplacePerson);
+app.MapDelete("/person/{id}", Handlers.DeletePerson);
 
 app.Run();
 
-public record Person(string FirstName, string LastName);
+public record Person(string FirstName, string LastName)
+{
+    public static readonly Dictionary<int, Person> All = new();
+}
+
+class Handlers
+{
+    public static void AddPerson(Person person)
+    {
+        Person.All.Add(Person.All.Count + 1, person);
+    }
+
+    public static void ReplacePerson(int id, Person person)
+    {
+        Person.All[id] = person;
+    }
+    public static void DeletePerson(int id)
+    {
+        Person.All.Remove(id);
+    }
+}
