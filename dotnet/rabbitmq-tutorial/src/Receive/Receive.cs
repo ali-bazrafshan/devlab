19296bcb1de60e214660398f505239df6d1dcff5
Ali Bazrafshan <alibazrafshan1707@gmail.com>
2026-02-15 14:35:37 +0330

    rabbitmq-tutorial: Set up a simple Hello World! program using RabbitMQ and C#

--- dotnet/rabbitmq-tutorial/src/Receive/Receive.cs
+++ dotnet/rabbitmq-tutorial/src/Receive/Receive.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+// Consumer also declares the queue, because it might not be created by a publisher yet
+await channel.QueueDeclareAsync(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Register a consumer
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"Received {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer
+);
+
+// Wait for messages
+Console.ReadLine();
--- dotnet/rabbitmq-tutorial/src/Send/Send.cs
+++ dotnet/rabbitmq-tutorial/src/Send/Send.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+// A connection represents a TCP connection between application and RabbitMQ broker
+using var connection = await factory.CreateConnectionAsync();
+// A channel represents a virtual connection. Each connection can create multiple channels
+using var channel = await connection.CreateChannelAsync();
+
+// Create a queue. Idempotent: declaring an already existing queue will do nothing
+await channel.QueueDeclareAsync(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Publish a message
+await channel.BasicPublishAsync(
+    exchange: string.Empty,
+    routingKey: "hello",
+    body: Encoding.UTF8.GetBytes("Hello World!")
+);
