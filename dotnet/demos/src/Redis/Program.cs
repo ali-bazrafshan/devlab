3342e9fe5fdefaca4865879dbd9511b7445bce75
Ali Bazrafshan <alibazrafshan1707@gmail.com>
2026-02-18 12:38:41 +0330

    dotnet-redis: Write a simple GET and SET with Redis API

---
+++ dotnet/demos/src/Redis/Program.cs
@@ -0,0 +1,8 @@
+﻿using StackExchange.Redis;
+
+var muxer = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+var db = muxer.GetDatabase();
+
+await db.StringSetAsync("testKey", "testValue");
+string? result = await db.StringGetAsync("testKey");
+Console.WriteLine(result);
