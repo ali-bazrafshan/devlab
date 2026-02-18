fbd6fca7e5ffb814e45fd155545a21939fb6a954
Ali Bazrafshan <alibazrafshan1707@gmail.com>
2026-02-18 17:26:47 +0330

    rabbitmq-tutorial: Create RPC client and server

---
+++ dotnet/rabbitmq-tutorial/src/RPCClient/RPCClient.cs
@@ -0,0 +1,134 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Concurrent;
+using System.Text;
+
+/// <summary>
+/// RPC client
+/// </summary>
+public class RpcClient : IAsyncDisposable
+{
+    private const string QUEUE_NAME = "rpc_queue";
+
+    private readonly IConnectionFactory _connectionFactory;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> _callbackMapper = new();
+
+    private IConnection? _connection;
+    private IChannel? _channel;
+    private string? _replyQueueName;
+
+    public RpcClient()
+    {
+        _connectionFactory = new ConnectionFactory { HostName = "localhost" };
+    }
+
+    /// <summary>
+    /// Start up the client
+    /// </summary>
+    /// <returns></returns>
+    public async Task StartAsync()
+    {
+        _connection = await _connectionFactory.CreateConnectionAsync();
+        _channel = await _connection.CreateChannelAsync();
+
+        // declare a server-named queue as a callback (reply) queue
+        QueueDeclareOk queueDeclareResult = await _channel.QueueDeclareAsync();
+        _replyQueueName = queueDeclareResult.QueueName;
+        var consumer = new AsyncEventingBasicConsumer(_channel);
+
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            // Since each client creates one callback queue, correlation ID is used to
+            // clarify which request each response belongs to
+            string? correlationId = ea.BasicProperties.CorrelationId;
+
+            if (!string.IsNullOrEmpty(correlationId))
+            {
+                if (_callbackMapper.TryRemove(correlationId, out var tcs))
+                {
+                    var body = ea.Body.ToArray();
+                    var response = Encoding.UTF8.GetString(body);
+                    tcs.TrySetResult(response);
+                }
+            }
+
+            return Task.CompletedTask;
+        };
+
+        await _channel.BasicConsumeAsync(_replyQueueName, true, consumer);
+    }
+
+    /// <summary>
+    /// Call RPC server
+    /// </summary>
+    /// <returns>RPC server's response</returns>
+    public async Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        if (_channel is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        // Client sends the message with two properties:
+        //  1. ReplyTo: callback queue
+        //  2. CorrelationId: unique value for every request
+        // This way each response will know excatly to what request it belongs 
+        string correlationId = Guid.NewGuid().ToString();
+        var props = new BasicProperties
+        {
+            CorrelationId = correlationId,
+            ReplyTo = _replyQueueName
+        };
+
+        var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+        _callbackMapper.TryAdd(correlationId, tcs);
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        await _channel.BasicPublishAsync(exchange: string.Empty, routingKey: QUEUE_NAME, mandatory: true, basicProperties: props, body: messageBytes);
+
+        using CancellationTokenRegistration ctr =
+            cancellationToken.Register(() =>
+            {
+                _callbackMapper.TryRemove(correlationId, out _);
+                tcs.SetCanceled();
+            });
+
+        return await tcs.Task;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_channel is not null)
+        {
+            await _channel.CloseAsync();
+        }
+
+        if (_connection is not null)
+        {
+            await _connection.CloseAsync();
+        }
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RPC Client");
+        string n = args.Length > 0 ? args[0] : "30";
+        await InvokeAsync(n);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string n)
+    {
+        var rpcClient = new RpcClient();
+        await rpcClient.StartAsync();
+
+        Console.WriteLine(" [x] Requesting fib({0})", n);
+        var response = await rpcClient.CallAsync(n);
+        Console.WriteLine(" [.] Got '{0}'", response);
+    }
+}
